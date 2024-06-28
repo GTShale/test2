3fa371acb11edc7f2b348db93c65cd55194a9323
GTShale <rus963333@gmail.com>
2024-06-28 14:40:34 +0500

    Lombok commit

--- TestStore/src/Order.java
+++ TestStore/src/Order.java
@@ -1,65 +1,11 @@
+import lombok.Getter;
+import lombok.Setter;
 
 public class Order {
-	private long id;
-	private boolean created;
-	private int statusId;
-	private int[] lastChanged;
-	private int totalPrice;
-	
-	//ID
-	public long getId() {
-		return id;
-	}
-	public void setId(long newId) {
-		this.id = newId;
-	}
-	
-	//userId
-	public void getUserId(User user) {
-        long userId = user.getId();
-	}
-	
-	//created
-	public boolean getCreated() {
-		return created;
-	}
-	public void setCreated(boolean newCreated) {
-		this.created = newCreated;
-	}
-	
-	//statusId
-	public int getStatusId() {
-		return statusId;
-	}
-	public void setStatusId(int newStatusId) {
-		this.statusId = newStatusId;
-	}
-	
-	//lastChanged
-	public int[] getLastChanged() {
-		return lastChanged;
-	}
-	public void setLastChanged(int[] newLastChanged) {
-		this.lastChanged = newLastChanged;
-	}
-	
-	//totalPrice
-	public int getTotalPrice() {
-		return totalPrice;
-	}
-	public void setTotalPrice(int newTotalPrice) {
-		this.totalPrice = newTotalPrice;
-	}
-	
-	//address
-	public String getUserAddress(UserAddress userAddress) {
-		String UserAddress = userAddress.getUserAddress();
-		return UserAddress;
-	}
-	
-	//phoneNumber
-	public int getPhoneNumber(UserPhoneNumber userPhoneNumber) {	
-            return userPhoneNumber.getPhoneNumber();
-	}
+	@Getter @Setter private long id;
+	@Getter @Setter private boolean created;
+	@Getter @Setter private int statusId;
+	@Getter @Setter private int[] lastChanged;
+	@Getter @Setter private int totalPrice;
 	
 }
--- TestStore/src/User.java
+++ TestStore/src/User.java
@@ -1,87 +1,15 @@
+import lombok.Getter;
+import lombok.Setter;
 
 public class User {
-	private long id;
-	private String login;
-	private char[] password;
-	private String firstName;
-	private String lastName;
-	private int birthday;
-	private boolean isBanned;
-	private boolean isDeleted;
-	private boolean isAdmin;
-	private String userAddress;
-	private int phoneNumber;
+	@Getter @Setter private long id;
+	@Getter @Setter private String login;
+	@Getter @Setter private char[] password;
+	@Getter @Setter private String firstName;
+	@Getter @Setter private String lastName;
+	@Getter @Setter private int birthday;
+	@Getter @Setter private boolean isBanned;
+	@Getter @Setter private boolean isDeleted;
+	@Getter @Setter private boolean isAdmin;
 	
-	//ID
-	public long getId(){
-		return id;
-	}
-	public void setId(long newId) {
-		this.id = newId;
-	}
-	
-	//login
-	public String getLogin(){
-		return login;
-	}
-	public void setLogin(String newLogin) {
-		this.login = newLogin;
-	}
-	
-	//password
-	public char[] getPassword() {
-	    return password;
-	}
-	public void setPassword(char[] newPassword) {
-	    this.password = newPassword;
-	}
-	
-	//firstName
-	public String getFirstName() {
-	    return firstName;
-	}
-	public void setFirstName(String newFirstName) {
-	    this.firstName = newFirstName;
-	}
-	
-	//lastName
-	public String getLastName() {
-	    return lastName;
-	}
-	public void setLastName(String newLastName) {
-	    this.lastName = newLastName;
-	}
-	
-	//birthday
-	public int getBirthday() {
-	    return birthday;
-	}
-	public void setBirthday(int newBirthday) {
-	    this.birthday = newBirthday;
-	}
-	
-	//isBanned
-	public boolean getIsBanned() {
-	    return isBanned;
-	}
-	public void setBanned(boolean banned) {
-	    this.isBanned = banned;
-	}
-	
-	//isDeleted
-	public boolean getIsDeleted() {
-	    return isDeleted;
-	}
-	public void setDeleted(boolean deleted) {
-	    this.isDeleted = deleted;
-	}
-	
-	//isAdmin
-	public boolean getIsAdmin() {
-	    return isAdmin;
-	}
-	public void setAdmin(boolean admin) {
-	    this.isAdmin = admin;
-	}
-
 }

8626a9bbd6cc8e017179bb664e03d348c18267cc
gshale <gshale@192.168.0.38>
2024-08-24 19:40:13 -0700

    1.6 testing push

--- NewStore/TestStore/src/main/app/UserRepository.java
+++ NewStore/TestStore/src/main/app/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import src.main.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	int testingunit = 2;
 }

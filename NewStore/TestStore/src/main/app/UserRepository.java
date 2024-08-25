8e2deb6808c3d68899eb304bae1a38f26a202b72
Arsen <rus963333@gmail.com>
2024-08-24 20:32:05 -0700

    1.5 all in app

--- NewStore/TestStore/src/main/app/UserRepository.java
+++ NewStore/TestStore/src/main/app/UserRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import src.main.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	int testingunit = 2;
 }

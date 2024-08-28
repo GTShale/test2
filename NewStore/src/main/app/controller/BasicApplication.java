f263827a9e6e37ac89d7117e12ee5fde493ca8d3
Arsen <rus963333@gmail.com>
2024-08-28 13:42:49 -0700

    Intellij IDEA1

--- NewStore/src/main/app/controller/BasicApplication.java
+++ NewStore/src/main/app/controller/BasicApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BasicApplication {
-	//justtest
+	//justtest1
 	public static void main(String[] args) {
 		
 		SpringApplication.run(BasicApplication.class, args);
